70d2a9407ab9d0053233ac16e043e72c40f667cf
Vpen <vpen1453313323@163.com>
2026-05-12 14:26:01 +0800

    feat: 新增页面

--- main.py
+++ main.py
@@ -99,7 +99,7 @@ logger.remove()
 logger.add(
     sys.stderr,
     level=LOG_LEVEL,
-    colorize=True,
+    colorize=None,
     format="<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <level>{level: <8}</level> | <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>"
 )
 
